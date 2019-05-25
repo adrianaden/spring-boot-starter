c0ece73242826d1afb3bd6e013f0dd4fe916721c
Adrian Adendrata <adrian.adendrata@doku.com>
2019-05-25 22:51:47 +0700

    Move InterceptorConfig

--- src/main/java/com/adrianaden/springboot/starter/config/InterceptorConfig.java
+++ src/main/java/com/adrianaden/springboot/starter/config/InterceptorConfig.java
@@ -1,11 +1,12 @@
-package com.adrianaden.springboot.starter.interceptor;
+package com.adrianaden.springboot.starter.config;
 
+import com.adrianaden.springboot.starter.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class Interceptor extends WebMvcConfigurerAdapter {
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
